40bbb20c7dc6bf91acca060600e4734fd19f4804
KovalchukPN <kovalchukpn@winelab.ru>
2021-08-19 12:43:15 +0300

    add add launch row layout remove wrong models

--- app/src/main/java/com/x5bartsoft/spacex/model/Telemetry.kt
+++
@@ -1,9 +0,0 @@
-package com.x5bartsoft.spacex.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Telemetry(
-    @SerializedName("flight_club")
-    val flightClub: Any
-)
--- app/src/main/java/com/x5bartsoft/spacex/model/Timeline.kt
+++
@@ -1,9 +0,0 @@
-package com.x5bartsoft.spacex.model
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Timeline(
-    @SerializedName("webcast_liftoff")
-    val webcastLiftoff: Int
-)
