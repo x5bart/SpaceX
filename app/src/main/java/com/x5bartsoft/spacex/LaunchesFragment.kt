d0f2c0d79ea0f508495934f14a1b4e3773515130
KovalchukPN <kovalchukpn@winelab.ru>
2021-08-19 15:18:59 +0300

    add shimmer placeholder

--- app/src/main/java/com/x5bartsoft/spacex/LaunchesFragment.kt
+++ app/src/main/java/com/x5bartsoft/spacex/LaunchesFragment.kt
@@ -5,17 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.x5bartsoft.spacex.databinding.FragmentLaunchesBinding
 
 
 class LaunchesFragment : Fragment() {
 
+    private var _binding: FragmentLaunchesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_launches, container, false)
+        _binding = FragmentLaunchesBinding.inflate(inflater, container, false)
+
+        showShimmerEffect()
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+
+    private fun showShimmerEffect() {
+        binding.fLaunchesShimmerLayout.visibility = View.VISIBLE
+        binding.fLaunchesShimmerLayout.startShimmer()
+        binding.fLaunchesRecyclerView.visibility = View.GONE
+    }
+
+    private fun hideShimmerEffect() {
+        binding.fLaunchesShimmerLayout.visibility = View.GONE
+        binding.fLaunchesShimmerLayout.stopShimmer()
+        binding.fLaunchesRecyclerView.visibility = View.VISIBLE
+
     }
 
 
